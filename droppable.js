934c5c3f1187306fa2153bb5bd41ec4dd64f07c6
Kory Nunn <knunn187@gmail.com>
2014-08-03 15:36:05 +1000

    Initial commit

---
+++ droppable.js
@@ -0,0 +1,25 @@
+var Gaffa = require('gaffa'),
+    Container = require('gaffa-container'),
+    grabetha = require('grabetha'),
+    crel = require('crel');
+
+function Droppable(){}
+Droppable = Gaffa.createSpec(Droppable, Container);
+Droppable.prototype.render = function() {
+    Container.prototype.render.call(this);
+
+    var element = this.renderedElement,
+        view = this;
+
+    var droppable = grabetha.droppable(element);
+
+    droppable.on('drop', function(event){
+
+        view.triggerActions('drop', {
+            data: event.grabbable.currentGrab.data
+        }, event);
+
+    });
+};
+
+module.exports = Droppable;
---
+++ grabbable.js
@@ -0,0 +1,32 @@
+var Gaffa = require('gaffa'),
+    Container = require('gaffa-container'),
+    grabetha = require('grabetha'),
+    crel = require('crel');
+
+function Grabbable(){}
+Grabbable = Gaffa.createSpec(Grabbable, Container);
+Grabbable.prototype.render = function() {
+    Container.prototype.render.call(this);
+
+    var element = this.renderedElement,
+        view = this;
+
+    var grabbable = grabetha.grabbable(element);
+
+    grabbable.on('grab', function(grab){
+
+        this.ghost = this.createGhost();
+
+        grab.on('move', function(){
+            this.data = view.data.value;
+        });
+
+    })
+    .on('drop', function(position){
+        this.ghost.destroy();
+        this.ghost = null;
+    });
+};
+Grabbable.prototype.data = new Gaffa.Property();
+
+module.exports = Grabbable;
